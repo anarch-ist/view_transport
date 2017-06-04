ce625513c0767d276482fc85448c754904058e77
kurbanov <kurbanov@addd3fad-e665-e511-a2ed-94de80a666d0>
2017-06-04 22:23:28 +0000

    [NF] add to user role 'temp_removed' admin page access restriction.

--- web_viewer/trunk/sbat.logist.ru/common_files/dao/userDao/Data.php
+++ web_viewer/trunk/sbat.logist.ru/common_files/dao/userDao/Data.php
@@ -5,7 +5,10 @@ include_once __DIR__ . '/../entityData/EntityData.php';
 
 class UserData extends EntityData
 {
-
+    function canAccessAdminPage() {
+        $userRole = $this->getData('userRoleID');
+        return !($userRole === "CLIENT_MANAGER" || $userRole === "TEMP_REMOVED");
+    }
 }
 
 class PermissionsData extends EntityData
