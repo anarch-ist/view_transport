c8f5bf32caa0039ac4a73184cce1cf9e593816b6
kurbanov <kurbanov@addd3fad-e665-e511-a2ed-94de80a666d0>
2017-06-04 21:59:07 +0000

    [NF] add to user role 'client_manager' admin page access restriction.

--- web_viewer/trunk/sbat.logist.ru/admin_page/content/php_files/main.php
+++ web_viewer/trunk/sbat.logist.ru/admin_page/content/php_files/main.php
@@ -4,6 +4,10 @@ include_once __DIR__ . '/../../../common_files/privilegedUser/PrivilegedUser.php
 $privUser = null;
 try {
     $privUser = PrivilegedUser::getInstance();
+    if ($privUser->getUserInfo()->getData('userRoleID') === "CLIENT_MANAGER") {
+        header("Location: ../", true, 303);
+        return;
+    }
     ?>
     <!DOCTYPE html>
     <html>
