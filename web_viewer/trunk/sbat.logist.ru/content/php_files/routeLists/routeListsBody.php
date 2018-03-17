c485b0e26127870b5e805b270003ea179ee1b888
kurbanov <kurbanov@addd3fad-e665-e511-a2ed-94de80a666d0>
2018-03-17 20:24:46 +0000

    routeListsPage added

---
+++ web_viewer/trunk/sbat.logist.ru/content/php_files/routeLists/routeListsBody.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <!--    Applying all the CSS-->
+    <link rel="stylesheet" type="text/css" href="common_files/media/Bootstrap-3.3.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="content/css/routeLists.css">
+    <link rel="stylesheet" type="text/css"
+          href="common_files/media/DataTables-1.10.12/css/datatables.bootstrap.css"/>
+    <link rel="stylesheet" type="text/css"
+          href="common_files/media/DataTables-1.10.12/css/jquery.dataTables.css"/>
+    <link rel="stylesheet" type="text/css" href="common_files/media/Editor-1.6.3/css/editor.jqueryui.min.css"/>
+    <link rel="stylesheet" type="text/css" href="common_files/media/font-awesome/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css" href="common_files/media/Buttons-1.2.2/css/buttons.bootstrap.css"/>
+<!--    <link rel="stylesheet" type="text/css" href="common_files/media/Buttons-1.2.2/css/buttons.dataTables.css"/>-->
+    <link rel="stylesheet" type="text/css" href="common_files/media/Select-1.2.0/css/select.jqueryui.min.css"/>
+    <!--    Custom css-->
+
+
+    <!--    Applying JS-->
+    <script src="common_files/media/jQuery-2.1.4/jquery-2.1.4.min.js" type="text/javascript"></script>
+    <script src="common_files/media/DataTables-1.10.10/js/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script src="common_files/media/Buttons-1.2.2/js/dataTables.buttons.js"></script>
+    <script src="common_files/media/Buttons-1.2.2/js/buttons.bootstrap.js"></script>
+    <script src="common_files/media/Buttons-1.2.2/js/buttons.html5.js"></script>
+    <script src="common_files/media/Select-1.2.0/js/dataTables.select.min.js"></script>
+    <!--        <script src="common_files/media/jQueryUI-1.11.4/jquery-ui.min.js"></script>-->
+    <script src="common_files/media/Bootstrap-3.3.2/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="common_files/media/jquery-mask/dist/jquery.mask.min.js" type="text/javascript"></script>
+    <script src="content/js/routeListsPage.js" type="text/javascript"></script>
+
+    <!--    Custom JS-->
+<!--    <script src="content/js/documentsWindow.js" type="text/javascript"></script>-->
+
+    <!--    <script src="common_files/media/DataTables-1.10.10/js/dataTables.jqueryui.min.js" type="text/javascript"></script>-->
+    <title>Маршрутные листы</title>
+
+</head>
+<body>
+<div class="container">
+    <table  id="routeListsTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+        <tr>
+            <th>Внешний ID</th>
+            <th>Номер маршрутного листа</th>
+            <th>Источник</th>
+            <th>Статус</th>
+            <th>Время отправки</th>
+            <th>Дата создания</th>
+        </tr>
+        </thead>
+    </table>
+
+</body>
+
+</html>
