e21fbd2640026f6f013a8148b9e1451d81ccc1fd
kurbanov <kurbanov@addd3fad-e665-e511-a2ed-94de80a666d0>
2018-03-18 10:53:48 +0000

    routeListsPage added

---
+++ web_viewer/trunk/sbat.logist.ru/content/js/routeListsPage.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+    var dataTable = $('#routeListsTable').DataTable({
+        processing: true,
+        search: {
+            caseInsensitive: true
+        },
+        fixedColumns: {
+            leftColumns: 1
+        },
+        ajax: {
+            url: "content/getData.php", // json datasource
+            type: "post",  // method  , by default get
+            data: {"status": "getRouteListsData"}
+        },
+        columnDefs: [
+            {"name": "routeListIDExternal", "data": "routeListIDExternal", "targets": 0},
+            {"name": "routeListNumber", "data": "routeListNumber", "targets": 1},
+            {"name": "dataSourceName", "data": "dataSourceName", "targets": 2},
+            {"name": "routeListStatusRusName", "data": "routeListStatusRusName", "targets": 3},
+            {"name": "departureDate", "data": "departureDate", "targets": 4},
+            {"name": "creationDate", "data": "creationDate", "targets":5}
+            // {"name": "routeListNumber", "data": "routeListNumber", "targets": 4, visible: false},
+            // {"name": "boxQty", "data": "boxQty", "targets": 5, visible: false}
+        ],
+        language: {
+            url: '/localization/dataTablesRus.json'
+        },
+        dom: 'Bfrtip',
+        // responsive: true,
+        select: {
+            style: 'single'
+        },
+        buttons: [
+            {
+
+                text: "На главную",
+                action: function (e, dt, node, config) {
+                    var url =
+                        "index.php";
+                    url = encodeURI(url);
+                    window.open(url);
+                }
+            },
+            {
+
+                text: "Заявки на этом МЛ",
+                extend: 'selectedSingle',
+                action: function (e, dt, node, config) {
+                    var url =
+                        "?routeListId=" +
+                        dataTable.row($('.selected')).data().routeListID;
+                    url = encodeURI(url);
+                    window.open(url);
+                }
+            }
+
+
+        ]
+    });
+
+});
